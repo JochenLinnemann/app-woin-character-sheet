26e289cd0c810548d872963d63f74f4f98a90627
Jochen Linnemann <jl@jochenlinnemann.de>
2024-03-05 19:52:19 +0100

    Add tempstate

---
+++ src/WoinRules/Characters/Attribute.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+
+namespace WoinRules.Characters;
+
+public record ProvidedStat<TStat, TProvider>(TStat Stat, TProvider Provider);
+
+/// <summary>
+///     Class Attribute.
+///     Implements the <see cref="IEquatable{Attribute}" />
+/// </summary>
+/// <param name="Name">The name of the attribute, e.g., "Strength"</param>
+/// <param name="ShortName">The short name of the attribute, e.g., "STR"</param>
+/// <param name="StartValue">The start value this attribute normally has, 3 for most attributes.</param>
+/// <param name="Value">The actual value of the attribute.</param>
+/// <seealso cref="IEquatable{Attribute}" />
+public record Attribute(string Name, string ShortName, int StartValue = 3, int Value = 3)
+{
+    private ImmutableArray<ProvidedStat<AttributeAdjustment, IAttributeAdjustmentProvider>> _adjustments = [];
+
+    public Attribute Apply(AttributeAdjustment adjustment, IAttributeAdjustmentProvider providedBy)
+    {
+        if (adjustment.ShortName != ShortName)
+            return this;
+
+        var adjustments = _adjustments.Add(
+            new ProvidedStat<AttributeAdjustment, IAttributeAdjustmentProvider>(adjustment, providedBy));
+        var value = StartValue + adjustments.Sum(a => a.Stat.Value);
+
+        return this with
+        {
+            _adjustments = adjustments,
+            Value = value
+        };
+    }
+
+    public Attribute Remove(AttributeAdjustment adjustment, IAttributeAdjustmentProvider providedBy)
+    {
+        if (adjustment.ShortName != ShortName)
+            return this;
+
+        var adjustments = _adjustments.Remove(
+            new ProvidedStat<AttributeAdjustment, IAttributeAdjustmentProvider>(adjustment, providedBy));
+        var value = StartValue + adjustments.Sum(a => a.Stat.Value);
+
+        return this with
+        {
+            _adjustments = adjustments,
+            Value = value
+        };
+    }
+}
+
+public record AttributeAdjustment(string ShortName, int Value);
+
+public interface IAttributeAdjustmentProvider
+{
+}
---
+++ src/WoinRules/Characters/Character.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Immutable;
+
+namespace WoinRules.Characters;
+
+public interface IStatProvider
+{
+}
+
+public interface IProvidedStat
+{
+    public IStatProvider Provider { get; }
+}
+
+public record Character
+{
+    public Age? Age { get; init; }
+
+    public ImmutableArray<Attribute> Attributes { get; init; }
+
+    public ImmutableArray<Career> Careers { get; init; }
+
+    public ImmutableArray<DerivedStatistic> DerivedStatistics { get; init; }
+
+    public Descriptor? Descriptor { get; init; }
+
+    public ImmutableArray<Equipment> Equipment { get; init; }
+
+    public ImmutableArray<Exploit> Exploits { get; init; }
+
+    public Heritage? Heritage { get; init; }
+
+    public ImmutableArray<Skill> Skills { get; init; }
+
+    public Trait? Trait { get; init; }
+
+    public static Character Create()
+    {
+        return new Character
+        {
+            Attributes =
+            [
+                new Attribute("Strength", "STR"),
+                new Attribute("Agility", "AGI"),
+                new Attribute("Endurance", "END"),
+
+                new Attribute("Intuition", "INT"),
+                new Attribute("Logic", "LOG"),
+                new Attribute("Willpower", "WIL"),
+
+                new Attribute("Charisma", "CHA"),
+                new Attribute("Luck", "LUC"),
+                new Attribute("Reputation", "REP"),
+
+                new Attribute("Power", "POW", 0, 0)
+            ]
+        };
+    }
+
+    public Character SetHeritage(Heritage heritage, Skill heritageSkill1, Skill heritageSkill2, Skill heritageSkill3)
+    {
+        var skills = Skills;
+        var exploits = Exploits;
+
+        if (Heritage is not null)
+        {
+            skills = skills.RemoveRange(Heritage.Skills);
+            exploits = exploits.RemoveRange(Heritage.Exploits);
+        }
+
+        skills = skills.AddRange(heritageSkill1, heritageSkill2, heritageSkill3);
+        exploits = exploits.AddRange(heritage.Exploits);
+
+        return this with
+        {
+            Heritage = heritage,
+            // TODO: apply attribute adjustments
+            Skills = skills,
+            Exploits = exploits
+        };
+    }
+}
+
+public record Heritage
+{
+    public ImmutableArray<AttributeAdjustment> AttributeAdjustments { get; init; }
+
+    public ImmutableArray<Exploit> Exploits { get; init; }
+
+    public ImmutableArray<Skill> Skills { get; init; }
+}
+
+public record Hook
+{
+}
+
+public record Descriptor
+{
+    public Hook? Hook { get; init; }
+}
+
+public record Career
+{
+}
+
+public record Exploit
+{
+}
+
+public record Trait
+{
+}
+
+public record Age
+{
+}
+
+public record DerivedStatistic
+{
+}
+
+public record Equipment
+{
+}
